856ecfcda21839cc14195dc5362258d4ca2d4ff9
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2026-08-14 10:39:23 -0300

    estructurta hecha

--- sirena-dashboard/app/page.tsx
+++ sirena-dashboard/app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+      </style>
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert h-5 w-[100px]"
--- sirena-dashboard/components/Header.tsx
+++ sirena-dashboard/components/Header.tsx
@@ -2,10 +2,11 @@ import Link from "next/link";
 
 
 const styles ={
-    header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 2rem', backgroundColor: '#333', color: '#fff' },
+    header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 2rem', backgroundColor: '#fff', color: '#333' },
     logo: { fontSize: '1.5rem', fontWeight: 'bold' },
-    nav: { display: 'flex', gap: '1.5rem' },
-    link: { color: '#fff', textDecoration: 'none' },
+    nav: { display: 'flex', gap: '3.5rem' },
+    nav2: { display: 'flex', gap: '3.5rem'},
+    link: { color: '#333', textDecoration: 'none' },
 }
 export default function Header() {
     return (
@@ -14,9 +15,12 @@ export default function Header() {
                 <Link href="/">MyLogo</Link>
             </div>
             <nav style={styles.nav}>
-                <Link href="/about" style={styles.link}>About</Link>
-                <Link href="/services" style={styles.link}>Services</Link>
-                <Link href="/contact" style={styles.link}>Contact</Link>
+                <Link href="/inicio" style={styles.link}>Inicio</Link>
+                <Link href="/mapa" style={styles.link}>Mapa</Link>
+            </nav>
+            <nav style={styles.nav2}>
+                <Link href="/register" style={styles.link}>Registrarse</Link>
+                <Link href="/login" style={styles.link}>Iniciar Sesión</Link>
             </nav>
         </header>
     );
