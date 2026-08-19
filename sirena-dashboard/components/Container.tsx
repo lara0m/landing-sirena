ba1b75d89a576ba2384c8ae572991678b4af60d6
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2026-08-19 08:57:54 -0300

    imagen funciona!

--- sirena-dashboard/components/Container.tsx
+++ sirena-dashboard/components/Container.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-import fondoImg from '@/public/fondo.png'
 
 const styles ={
     header: { display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '1rem 2rem', color: '#fff' },
@@ -14,9 +13,14 @@ export default function Container() {
     return (
         <body>
             <Image
-            src={fondoImg}
+            src="/fondo.png"
             alt="Imagen de fondo"
-            fill={true}
+
+            width={1920}
+            height={1080}
+            priority
+            quality={75}
+            style={{ objectFit: "cover", width: "100%", height: "100vh" }}
             loading="eager"
             className="fondo"
             />
